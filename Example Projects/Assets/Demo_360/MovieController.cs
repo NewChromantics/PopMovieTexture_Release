de5f2b4ae1cab0a5ace6483daa6faffe7424692d
Graham Reeves <graham@grahamreeves.com>
2016-01-20 17:11:40 +0000

    Initial 360 movie renderer implemented. Gyro doesn't work well on android. Has a GUI which when the button is clicked starts a movie on the sphere

---
+++ Example Projects/Assets/Demo_360/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+
+	public MeshRenderer			mTarget;
+	public PopMovie				mMovie;
+	public string				mFilename;
+	public Texture				mTargetTexture;
+
+	public void StartMovie()
+	{
+		var Params = new PopMovieParams ();
+		mMovie = new PopMovie (mFilename, Params, true);
+		mMovie.AddDebugCallback (Debug.Log);
+	}
+
+	void Update()
+	{
+		if (mMovie != null)
+			mMovie.Update ();
+		if (mMovie != null && mTargetTexture!=null)
+			mMovie.UpdateTexture(mTargetTexture);
+
+		//	waiting for first frame
+		if (!HaveAppliedTexture () && mMovie!=null ) {
+			var LastFrameCopied = mMovie.GetLastFrameCopiedMs ();
+			if (LastFrameCopied != 0)
+				OnMovieFrameReady ();
+		}
+	}
+
+	bool HaveAppliedTexture()
+	{
+		return mTarget.material.mainTexture == mTargetTexture;
+	}
+
+	void OnMovieFrameReady()
+	{
+		mTarget.material.mainTexture = mTargetTexture;
+	}
+
+}

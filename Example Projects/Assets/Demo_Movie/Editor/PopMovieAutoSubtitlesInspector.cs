8776627c7c9ee76299f1ed33c658060f91e90d40
Graham Reeves <graham@grahamreeves.com>
2016-03-31 16:04:22 +0100

    Added Demo_Movie which plays a clip of Dr Strangelove, with automatic subtitle detection & a speed slider

---
+++ Example Projects/Assets/Demo_Movie/Editor/PopMovieAutoSubtitlesInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+/*
+	Example of how to use the inspector on a custom type
+*/
+[CustomEditor(typeof(PopMovieAutoSubtitles))]
+public class PopMovieAutoSubtitlesInspector : PopMovieInspector
+{
+	public override void OnInspectorGUI()
+	{
+		var Object = target as PopMovieAutoSubtitles;
+		ShowInspectorDebug( Object.Instance, this );
+		DrawDefaultInspector();
+	}
+}
+
